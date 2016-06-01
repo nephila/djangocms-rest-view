f988d7aa18bae34ced4f47ba7059e6b8ddc07081
Iacopo Spalletti <i.spalletti@nephila.it>
2016-06-01 11:53:12 +0200

    Add copyright notice for RecursiveField

--- djangocms_rest_view/fields.py
+++ djangocms_rest_view/fields.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
+""""
+Copyright (c) 2015, Warren Jin <jinwarren@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
 
 import inspect
 import importlib
@@ -10,6 +25,10 @@ from rest_framework.serializers import BaseSerializer
 class RecursiveField(Field):
     """
     A field that gets its representation from its parent.
+
+    This is taken from https://github.com/heywbj/django-rest-framework-recursive
+    vendorized here for easier deployment. All copyright on this file belongs to the
+    author Warren Jin <jinwarren@gmail.com>.
 
     This method could be used to serialize a tree structure, a linked list, or
     even a directed acyclic graph. As with all recursive things, it is
